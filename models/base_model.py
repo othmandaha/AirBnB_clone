7fc9e0e62b0197168e9dcb306f9cc4951b79d1d7
othmandaha <dahaothman02@gmail.com>
2024-03-08 18:11:54 +0100

    Storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -4,46 +4,44 @@ from datetime import datetime
 import uuid
 import models
 
+
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
-        """Represents the BaseModel of the HBnB project."""
-        def __init__(self, *args, **kwargs):
-                """Initialization of a Base instance.
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
 
-                Args:
-                        - *args: list of arguments
-                        - **kwargs: dict of key-values arguments
-                """
-                form = "%Y-%m-%dT%H:%M:%S.%f"
-                if len(kwargs) != 0:
-                        for k, v in kwargs:
-                                if k == "created_at" or k == "updated_at":
-                                        self.__dict__[k] = datetime.strptime(v, form)
-                                else:
-                                        self.__dict__[k] = v
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, date_format)
                 else:
-                        self.id = str(uuid.uuid4())
-                        self.created_at = datetime.now()
-                        self.updated_at = datetime.now()
+                    self.__dict__[k] = v
+        else:
+            models.storage.save()
 
-        def __str__(self):
-                """Return the print/str representation of the BaseModel instance."""
-                cla_name = self.__class__.__name__
-                return "[{}] ({}) {}".format(cla_name, self.id, self.__dict__)
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
-        def save(self):
-                """Updates the updated_at attribute."""
-                self.updated_at = datetime.now()
+    def save(self):
+        """Updates the updated_at attribute."""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
-        def to_dict(self):
-                """Returns a dictionary representation of an attribute."""
-
-                the_dict = self.__dict__.copy()
-                the_dict["__class__"] = self.__class__.__name__
-                the_dict["created_at"] = the_dict["created_at"].isoformat()
-                the_dict["updated_at"] = the_dict["updated_at"].isoformat()
-                return the_dict
-
-        
-
-
+    def to_dict(self):
+        """Returns a dictionary representation of an attribute."""
+        the_dict = self.__dict__.copy()
+        the_dict["__class__"] = self.__class__.__name__
+        the_dict["created_at"] = the_dict["created_at"].isoformat()
+        the_dict["updated_at"] = the_dict["updated_at"].isoformat()
+        return the_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+"""Defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns dictionary representation of objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        c_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(c_name, obj.id)] = obj
+
+    def save(self):
+        """serialize __objects to __file_path."""
+        the_dict = FileStorage.__objects
+        form_dict = {obj: the_dict[obj].to_dict() for obj in the_dict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(form_dict, f)
+
+    def reload(self):
+        """deserialize __file_path to __obejcts if it exists."""
+        try:
+            with open(FileStorage.__file_path, "r") as f:
+                objects_dict = json.load(f)
+                for o in objects_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
